16247fb5e76a715f268386d1da42f0764c0d011e
Josef Bacik <jbacik@fb.com>
2017-03-30 16:05:42 -0400

    initial checkin

---
+++ test-destroy-on-disconnect.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+insmod ~/btrfs-next/drivers/block/nbd.ko nbds_max=0
+pkill -9 nbd-server
+~/nbd/nbd-server -C ~/nbd/server.conf
+~/nbd/nbd-client -L -e -t 5 -N fail -C 4 localhost &
+sleep 10
+#echo none > /sys/block/nbd0/queue/scheduler
+mkfs.btrfs -f /dev/nbd0
+mount /dev/nbd0 /mnt/btrfs-test
+~/fio/fio --randrepeat=1 --ioengine=libaio --direct=1 --gtod_reduce=1 \
+	--name=test --directory=/mnt/btrfs-test --nrfiles=48 --numjobs=12 \
+	--bs=4M --iodepth=256 --size=4800M --io_size=400G --readwrite=randrw \
+	--fsync=100 &
+sleep 5
+~/nbd/nbd-client -d /dev/nbd0
+stat /dev/nbd0 > /dev/null 2>&1
+[ $? -ne 0 ] && echo "WE FUCKED UP PART 1" && exit 1
+umount /mnt/btrfs-test
+stat /dev/nbd0 > /dev/null 2>&1
+[ $? -eq 0 ] && echo "WE FUCKED UP PART 2" && exit 1
+rmmod nbd
---
+++ test-reconnects.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+_random_disconnects() {
+	COUNT=0
+	sleep 60
+	while [ true ]
+	do
+		if [ $COUNT -eq 10 ]
+		then
+			echo "killing whole server"
+			pkill -9 nbd-server
+			sleep 1
+			~/nbd/nbd-server -C ~/nbd/server.conf
+			COUNT=0
+		else
+			PID=$(ps auxw | grep nbd-server | head -2 | tail -1 | awk '{ print $2 }')
+			echo "killing $(ps auxw | grep $PID)"
+			kill -9 $PID
+		fi
+		sleep 60
+		COUNT=$(expr $COUNT + 1)
+	done
+}
+
+echo $OPTS
+insmod ~/btrfs-next/drivers/block/nbd.ko
+pkill -9 nbd-server
+~/nbd/nbd-server -C ~/nbd/server.conf
+~/nbd/nbd-client -M -D 10 -L -t 5 -N fail -C 4 localhost /dev/nbd0 &
+sleep 10
+_random_disconnects &
+DISCONNECT_PID=$!
+#echo none > /sys/block/nbd0/queue/scheduler
+mkfs.btrfs -f /dev/nbd0
+mount /dev/nbd0 /mnt/btrfs-test
+~/fio/fio --randrepeat=1 --ioengine=libaio --direct=1 --gtod_reduce=1 \
+	--name=test --directory=/mnt/btrfs-test --nrfiles=48 --numjobs=12 \
+	--bs=4M --iodepth=256 --size=4800M --io_size=400G --readwrite=randrw \
+	--fsync=100
+umount /mnt/btrfs-test
+~/nbd/nbd-client -d /dev/nbd0
+pkill -9 $DISCONNECT_PID
+pkill -9 nbd-client
+rmmod nbd
